79a86631e28e92211f5e8ef7eb87cdd13ec23b5a
Paul Querna <pquerna@apache.org>
2010-09-04 18:32:04 -0700

    Simple reverse proxy server

--- limiting_concurrency/server_nolimit.js
+++ limiting_concurrency/server_nolimit.js
@@ -2,3 +2,27 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND. See the accompanying 
  * LICENSE file.
  */
+
+var http = require('http');
+var sys = require('sys');
+var destination = "nodejs.org";
+
+http.createServer(function(req, res) {
+  var proxy = http.createClient(80, destination);
+  var preq = proxy.request(req.method, req.url, req.headers);
+
+  preq.on('response', function(pres) {
+    res.writeHead(pres.statusCode, pres.headers);
+    sys.pump(pres, res);
+    pres.on('end', function() {
+      preq.close();
+      res.close();
+    })
+  });
+  req.on('data', function(chunk) {
+    preq.write(chunk, 'binary');
+  });
+  req.on('end', function() {
+    preq.end();
+  });
+}).listen(8080);
