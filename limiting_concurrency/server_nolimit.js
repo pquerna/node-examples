38a93675805bbca292f6d543b891a061b800e9bb
Paul Querna <pquerna@apache.org>
2010-09-04 18:37:22 -0700

    little nice things

--- limiting_concurrency/server_nolimit.js
+++ limiting_concurrency/server_nolimit.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /* This code is PUBLIC DOMAIN, and is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND. See the accompanying 
  * LICENSE file.
@@ -11,12 +12,13 @@ http.createServer(function(req, res) {
   var proxy = http.createClient(80, destination);
   var preq = proxy.request(req.method, req.url, req.headers);
 
+  console.log(req.connection.remoteAddress +" "+ req.method +" "+req.url);
   preq.on('response', function(pres) {
     res.writeHead(pres.statusCode, pres.headers);
     sys.pump(pres, res);
     pres.on('end', function() {
-      preq.close();
-      res.close();
+      preq.end();
+      res.end();
     })
   });
   req.on('data', function(chunk) {
